6dd992fd7a7cbb840bbdf6dd2289bcb4028200da
Haris <haris.chandra@pathaopay.com>
2024-02-17 01:50:33 +0600

    pytest group

--- pytestsDemo/test_demo1.py
+++ pytestsDemo/test_demo1.py
@@ -1,17 +1,26 @@
-
 # before run
 #   1. Edit Configuration > plus(+) sign > Python Test > Pytest
 #   2. If show error "No pytest runner found in selected interpreter" import pytest
 
 #   Run from terminal
 #   1. go to project folder(/Python/PythonTesting/pytestsDemo)
-#   2. run: py.test / py.test -v / py.test -v -s (v for more information and s for another console logs)
+#   2. run: py.test / py.test -v / py.test -v -s
+#   3. run: py.test file name (ex: py.test test_demo1.py) for run single file
+#   4. run: py.test -k method Name (ex : py.test -k Haris -v -s ) for run selected method
+#   5. run : py.test -m group name -v -s (py.test -m smoke -v -s) for run group wise
+
+# -k stands for method execution, -s logs in out put, -v stands for more information metadata
+
+import pytest
+
 
 def test_firstProgram():
     print("hello")
 
+@pytest.mark.smoke
 def test_firstMethods():
     print("first Methods")
 
+@pytest.mark.skip
 def test_Haris():
     print("print from demo 1")
--- pytestsDemo/test_demo2.py
+++ pytestsDemo/test_demo2.py
@@ -1,21 +1,24 @@
 # before run
 #   1. Edit Configuration > plus(+) sign > Python Test > Pytest
 #   2. If show error "No pytest runner found in selected interpreter" import pytest
+import pytest
+
 
 #   Run from terminal
 #   1. go to project folder(/Python/PythonTesting/pytestsDemo)
 #   2. run: py.test / py.test -v / py.test -v -s
 #   3. run: py.test file name (ex: py.test test_demo1.py) for run single file
 #   4. run: py.test -k method Name (ex : py.test -k Haris -v -s ) for run selected method
+#   5. run : py.test -m group name -v -s (py.test -m smoke -v -s) for run group wise
 
 # -k stands for method execution, -s logs in out put, -v stands for more information metadata
 
-
+@pytest.mark.xfail # failed but not in the report
 def test_secondMethods():
     message = 'second'
     assert message == "second Methods", "Test failed because condition do not match"
     # print("second Methods")
 
-
+@pytest.mark.smoke
 def test_Haris():
     print("print from demo 2")
